bd40ccca4949246f1bcab11eda37ebcff1938fa4
ss <tt@email.com>
2025-03-24 01:38:08 +0900

    chore: test 파일 원상복구

--- src/pages/wikilist.tsx
+++ src/pages/wikilist.tsx
@@ -1,9 +1,5 @@
-import { AlarmIcon } from '@/components/common/Icon/Icon';
-
-const WikiList = () => (
-  <div className="flex gap-6 j items-center bg-white p-6 rounded-lg shadow-lg">
-    <AlarmIcon size={24} className="cursor-pointer hover:scale-110 transition-transform" />
-  </div>
-);
+const WikiList = () => {
+  return <div>wikilist page</div>;
+};
 
 export default WikiList;
