2507681935c073e8775fab29721d8069682e6315
ss <tt@email.com>
2025-03-24 00:49:36 +0900

    feat: @svgr/webpack 라이브러리 설치 및 설정

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -4,13 +4,24 @@ import Image from 'next/image';
 interface IconProps extends React.HTMLAttributes<HTMLSpanElement> {
   name: string;
   size?: number;
+  hoverEffect?: 'scale' | 'opacity' | 'all' | 'none';
   className?: string;
 }
 
-const Icon = ({ name, size = 24, className = '', ...props }: IconProps) => {
+const hoverVariants = {
+  scale: 'hover:scale-110 transition-transform duration-200',
+  opacity: 'hover:opacity-75 transition-opacity duration-200',
+  all: 'hover:scale-110 hover:opacity-75 transition-all duration-200',
+
+  none: '',
+};
+
+const Icon = ({ name, size = 24, hoverEffect = 'none', className = '', ...props }: IconProps) => {
+  const hoverClass = hoverVariants[hoverEffect];
+
   return (
     <span
-      className={`inline-flex items-center justify-center  ${className}`}
+      className={`inline-flex items-center justify-center ${hoverClass} ${className}`}
       style={{ width: size, height: size }}
       {...props}
     >
