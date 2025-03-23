821b295e5532c96f30832d784c1180ee74381af2
ss <tt@email.com>
2025-03-24 00:01:04 +0900

    docs: 버튼 컴포넌트 테스트 페이지 원상복구

--- src/pages/wikilist.tsx
+++ src/pages/wikilist.tsx
@@ -1,20 +1,5 @@
-import Button from '@/components/common/Button';
-
 const WikiList = () => {
-  return (
-    <div className=" flex-col flex gap-3 items-center justify-center min-h-screen bg-white text-black p-6">
-      <Button buttonText="내 위키 만들기" />
-      <Button width="400px" height="45px" buttonText="로그인" />
-
-      <Button variant="secondary" buttonText="내 위키 만들기" />
-      <Button variant="secondary" width="400px" buttonText="로그인" />
-
-      <Button loading buttonText="내 위키 만들기" />
-      <Button loading width="400px" buttonText="로그인" />
-
-      <Button variant="primary" width="110px" loading buttonText="편집 중" showLoadingImage />
-    </div>
-  );
+  return <div>wikilist page</div>;
 };
 
 export default WikiList;
