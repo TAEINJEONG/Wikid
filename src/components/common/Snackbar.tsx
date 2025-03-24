2a3d86b23955949d2bf50b34650c1a6e29c9d9ca
ss <tt@email.com>
2025-03-24 19:34:33 +0900

    feat: snackbar 기능 구현, 수정 예정

--- src/components/common/Snackbar.tsx
+++ src/components/common/Snackbar.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { RedErrorIcon, GrayInfoIcon, GreenCheckIcon } from './Icons/Icons';
 
 type SnackbarType = 'gray' | 'green' | 'red';
 
 interface SnackbarProps {
   type?: SnackbarType;
   message: string;
+  duration?: number; // ms 단위
   onClose?: () => void;
+  iconSize?: number;
 }
 
 const typeStyles = {
   gray: {
     bg: 'bg-gray-50',
     text: 'text-gray-500',
+    Icon: GrayInfoIcon,
   },
   green: {
     bg: 'bg-green-100',
     text: 'text-green-300',
+    Icon: GreenCheckIcon,
   },
   red: {
     bg: 'bg-red-200',
     text: 'text-red-200',
+    Icon: RedErrorIcon,
   },
 };
 
-const Snackbar: React.FC<SnackbarProps> = ({ type = 'gray', message, onClose }) => {
-  const { bg, text } = typeStyles[type];
+const Snackbar: React.FC<SnackbarProps> = ({
+  type = 'gray',
+  message,
+  duration = 3000,
+  onClose,
+  iconSize = 20,
+}) => {
+  const { bg, text, Icon } = typeStyles[type];
+
+  useEffect(() => {
+    if (onClose) {
+      const timer = setTimeout(onClose, duration);
+      return () => clearTimeout(timer);
+    }
+  }, [duration, onClose]);
 
   return (
     <div
@@ -37,8 +56,8 @@ const Snackbar: React.FC<SnackbarProps> = ({ type = 'gray', message, onClose }) 
         maxWidth: '400px',
       }}
     >
+      <Icon size={iconSize} className={text} />
       <span className="flex-1">{message}</span>
-      {onClose && <button onClick={onClose}></button>}
     </div>
   );
 };
--- src/pages/wikilist.tsx
+++ src/pages/wikilist.tsx
@@ -1,5 +1,32 @@
+import Snackbar from '@/components/common/Snackbar';
+import { useState } from 'react';
+
 const WikiList = () => {
-  return <div>wikilist page</div>;
+  const [showSnackbar, setShowSnackbar] = useState(false);
+
+  return (
+    <div className="w-full h-screen bg-white flex flex-col items-center justify-center">
+      <button
+        className="bg-green-200 text-white px-4 py-2 rounded"
+        onClick={() => setShowSnackbar(true)}
+      >
+        스낵바 띄우기
+      </button>
+
+      {/* 스낵바는 하단 고정 위치로 띄움 */}
+      {showSnackbar && (
+        <div className="fixed bottom-6 right-6 z-50">
+          <Snackbar
+            type="green"
+            message="저장이 완료되었습니다!"
+            duration={3000}
+            iconSize={20}
+            onClose={() => setShowSnackbar(false)}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default WikiList;
