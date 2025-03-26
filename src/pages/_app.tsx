767cae2bb1ecbfbc27dece3b9c4127ae989bc9e4
ss <tt@email.com>
2025-03-26 20:57:16 +0900

    chore: SnackbarContext.tsx 를 lib/context/ 폴더로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { SnackbarProvider } from '@/context/SnackbarContext';
+import { SnackbarProvider } from '@/lib/context/SnackbarContext';
 import { AuthProvider } from '@/lib/context/authProvider';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
