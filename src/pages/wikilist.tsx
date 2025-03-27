7fba9135a8f29ce54669d7869359b1bb736dae65
mecrojun <wnsgh6588@naver.com>
2025-03-27 17:39:28 +0900

    style: 위키리스트 페이지 틀 생성 & chore: 위키리스트내 사용하는 컴포넌트 파일 생성

--- src/pages/wikilist.tsx
+++ src/pages/wikilist.tsx
@@ -1,5 +1,38 @@
+import axiosInstance from "@/lib/api/axios";
+import { useEffect, useState } from "react";
+
 const WikiList = () => {
-  return <div>wikilist page</div>
+  
+  const [cardList, setCardList] = useState<any>();
+
+  async function fetchGetProfiles() {
+    try{
+      const { data } = await axiosInstance.get('/profiles');
+      setCardList(data);
+      console.log(data);
+      console.log(cardList);
+    } catch {
+      console.log("데이터 불러오기 실패");
+    }
+  }
+
+  function showCardList() {
+    console.log(cardList);
+  }
+  
+  useEffect(() => {
+    fetchGetProfiles();
+  },[])
+
+  return (
+    <>
+      <div>헤더</div>
+      <div className="flex flex-col items-center">
+        <div>검색창</div>
+        <div>위키 카드 리스트</div>
+      </div>
+    </>
+  );
 }
 
 export default WikiList;
