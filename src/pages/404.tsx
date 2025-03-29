380e1c57f56d5725b0f31f85eeba8d85a520d4d1
mecrojun <wnsgh6588@naver.com>
2025-03-29 19:02:38 +0900

    style: 404페이지 틀 제작

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,17 @@
 const PageError = () => {
-  return <div>404 ErrorPage</div>
+  return (
+    <>
+    <div>헤더</div>
+      <div className="px-[10px] py-[144px] flex flex-col items-center">
+        <div className="w-[600px] h-[200px] mb-[20px] border border-red-500">404 찾을 수 없습니다 이미지</div>
+        <div className="font-pre text-2xl-b">페이지를 찾을 수 없습니다</div>
+        <div className="mt-[30px]">
+          <p className="font-pre text-lg-r">존재하지 않거나, 사용할 수 없는 페이지입니다</p>
+          <p className="font-pre text-lg-r">입력하신 주소가 맞는지 다시 확인 주세요</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PageError;
