4845c5434f08f89c725395e8e2e80eb3a2a3985b
ss <tt@email.com>
2025-03-31 18:09:59 +0900

    header UI 일단 완성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -13,10 +13,14 @@ import { useAuth } from '@/lib/context/authProvider';
 const Header = () => {
   const { isLoggedIn } = useAuth();
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+  const toggleProfileMenu = () => setProfileMenuOpen((prev) => !prev);
 
   return (
-    <header className="w-full h-[80px] bg-white shadow-md">
-      <div className="max-w-[1920px] mx-auto w-full h-full flex items-center justify-between px-5 py-2.5 lg:px-[80px] lg:py-[20px]">
+    <header className="w-full h-[79px] bg-white shadow-sm border-b-[1px] border-gray-100">
+      <div className="max-w-[1920px] mx-auto w-full h-full flex items-center justify-between px-5 py-2.5 lg:px-[80px] lg:py-[20px] text-sm leading-6">
         <nav className="flex items-center justify-between gap-[40px] mr-[165px]">
           <Link
             href="/"
@@ -29,13 +33,13 @@ const Header = () => {
           <nav className="hidden md:flex items-center gap-6">
             <Link
               href="/wikilist"
-              className="text-md font-medium hover:text-green-300"
+              className="text-md font-medium hover:text-gray-400"
             >
               위키목록
             </Link>
             <Link
               href="/boards"
-              className="text-md font-medium hover:text-green-300"
+              className="text-md font-medium hover:text-gray-400"
             >
               자유게시판
             </Link>
@@ -43,10 +47,40 @@ const Header = () => {
         </nav>
 
         <div className="hidden md:flex items-center gap-6">
-          {!isLoggedIn ? (
+          {isLoggedIn ? (
             <>
               <AlarmIcon size={24} className="cursor-pointer" />
-              <ProfileIcon size={24} className="cursor-pointer" />
+              <ProfileIcon
+                size={24}
+                className="cursor-pointer"
+                onClick={toggleProfileMenu}
+              />
+              {isProfileMenuOpen && (
+                <div className="absolute top-20 right-0 w-[110px] h-[131] rounded-md shadow-md lg:right-[60px] bg-gray-50 py-4 flex flex-col items-center gap-4  z-20">
+                  <Link
+                    href="/myPage"
+                    onClick={() => setProfileMenuOpen(false)}
+                    className="hover:text-gray-400"
+                  >
+                    계정 설정
+                  </Link>
+                  <Link
+                    href="/wiki[id]"
+                    onClick={() => setProfileMenuOpen(false)}
+                    className="hover:text-gray-400"
+                  >
+                    내 위키
+                  </Link>
+                  {/*로그아웃은 링크가 아니라 로그아웃 처리만 하면 되는지*/}
+                  <Link
+                    href="/myPage"
+                    onClick={() => setProfileMenuOpen(false)}
+                    className="text-gray-400 hover:text-gray-300"
+                  >
+                    로그아웃
+                  </Link>
+                </div>
+              )}
             </>
           ) : (
             <Link href="/login">
@@ -57,39 +91,23 @@ const Header = () => {
           )}
         </div>
 
-        <button className="md:hidden" onClick={() => setMenuOpen(!isMenuOpen)}>
+        <button className="md:hidden" onClick={toggleMenu}>
           <MenuIcon size={28} className="cursor-pointer hover:text-green-300" />
         </button>
 
         {isMenuOpen && (
           <div className="absolute top-20 right-0 w-[120px] h-[176] rounded-md shadow-md bg-white py-4 flex flex-col items-center gap-4 md:hidden z-20">
-            <Link
-              href="/wikilist"
-              onClick={() => setMenuOpen(false)}
-              className="text-lg font-medium"
-            >
+            <Link href="/wikilist" onClick={() => setMenuOpen(false)}>
               위키목록
             </Link>
-            <Link
-              href="/boards"
-              onClick={() => setMenuOpen(false)}
-              className="text-lg font-medium"
-            >
+            <Link href="/boards" onClick={() => setMenuOpen(false)}>
               자유게시판
             </Link>
 
-            <Link
-              href="/boards"
-              onClick={() => setMenuOpen(false)}
-              className="text-lg font-medium"
-            >
+            <Link href="/boards" onClick={() => setMenuOpen(false)}>
               알림
             </Link>
-            <Link
-              href="/myPage"
-              onClick={() => setMenuOpen(false)}
-              className="text-lg font-medium"
-            >
+            <Link href="/myPage" onClick={() => setMenuOpen(false)}>
               마이페이지
             </Link>
           </div>
