2fe249a0dc945fa10ad54dc70613e164680b6796
mecrojun <wnsgh6588@naver.com>
2025-03-31 16:42:40 +0900

    chore: 404페이지 오타 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -24,7 +24,7 @@ const PageError = () => {
       <div className="font-pre font-[700] text-[#4CBFA4]" style={{ fontSize: '36px' }}>페이지를 찾을 수 없습니다</div>
       <div className="mt-[40px] px-[16px] py-[24px] border-2 border-dashed border-[rgba(71,77,102,0.3)] rounded-[10px]">
         <p className="font-pre text-lg-sb text-[rgba(71,77,102,0.5)]">존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
-        <p className="font-pre text-lg-sb text-[rgba(71,77,102,0.5)]">입력하신 주소가 맞는지 다시 확인 주세요.</p>
+        <p className="font-pre text-lg-sb text-[rgba(71,77,102,0.5)]">입력하신 주소가 맞는지 다시 확인해 주세요.</p>
       </div>
       <div className="mt-[24px]">
         <Link href="/" className="px-4 py-2 bg-[#4CBFA4] font-pre text-white font-[48px] font-[500] rounded-lg shadow-lg hover:bg-[#3AA98E] transition-colors">
