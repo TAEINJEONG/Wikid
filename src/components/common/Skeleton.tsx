329c9003a58d8ddaf3e24686307b87f7af578a34
ss <tt@email.com>
2025-04-01 13:31:14 +0900

    chore: globals.css 원상복구 및 skeleton 속성 변경

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -3,21 +3,14 @@ import clsx from 'clsx';
 
 interface SkeletonProps {
   className?: string;
-  borderRadius?: string;
   as?: 'div' | 'span';
 }
 
 const Skeleton: React.FC<SkeletonProps> = ({
   className = '',
-  borderRadius = '8px',
   as: Tag = 'div',
 }) => {
-  return (
-    <Tag
-      className={clsx('bg-gray-100', 'skeleton-shimmer', className)}
-      style={{ borderRadius }}
-    />
-  );
+  return <Tag className={clsx('bg-gray-200', className)} />;
 };
 
 export default Skeleton;
