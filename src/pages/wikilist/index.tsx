7928fde78e58dcc5d91251ced974aab78ab15ce8
TAEINJEONG <taeinjeong46@gmail.com>
2025-04-02 16:29:52 +0900

    fix: wikilist 의존성 배열 수정

--- src/pages/wikilist/index.tsx
+++ src/pages/wikilist/index.tsx
@@ -1,9 +1,9 @@
-import { GraySearchIcon, SearchIcon } from "@/components/common/Icons";
-import Pagination from "@/components/common/Pagenation";
-import SearchBar from "@/components/common/SearchBar";
-import { Card } from "@/components/WikilistComponent";
-import axiosInstance from "@/lib/api/axios";
-import { useEffect, useState } from "react";
+import { GraySearchIcon, SearchIcon } from '@/components/common/Icons';
+import Pagination from '@/components/common/Pagenation';
+import SearchBar from '@/components/common/SearchBar';
+import { Card } from '@/components/WikilistComponent';
+import axiosInstance from '@/lib/api/axios';
+import { useCallback, useEffect, useState } from 'react';
 
 interface cardProps {
   id: number;
@@ -16,43 +16,50 @@ interface cardProps {
 }
 
 const WikiList = () => {
-  
   const [cardList, setCardList] = useState<cardProps[]>([]);
   const [initialCardList, setInitialCardList] = useState<cardProps[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPage, setTotalPage] = useState<number>(1);
 
-  const [keyWord, setkeyWord] = useState<string>("");
+  const [keyWord, setkeyWord] = useState<string>('');
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   async function fetchGetProfiles() {
-    try{
+    try {
       const { data } = await axiosInstance.get('/profiles?pageSize=20');
       setIsLoading(false);
       setCardList(data.list);
       setInitialCardList(data.list);
-      setTotalPage(Math.ceil(data.totalCount/3));
+      setTotalPage(Math.ceil(data.totalCount / 3));
     } catch {
-      alert("데이터 불러오기 실패");
+      alert('데이터 불러오기 실패');
     }
   }
 
-  function fetchSearchProfiles() {
-    if(keyWord === '')
-      setCardList(initialCardList);
+  const fetchSearchProfiles = useCallback(async () => {
+    if (keyWord === '') setCardList(initialCardList);
     else
-      setCardList(initialCardList.filter((profile) => profile.name.includes(keyWord)));
-  }
+      setCardList(
+        initialCardList.filter((profile) => profile.name.includes(keyWord))
+      );
+  }, [initialCardList, keyWord]);
 
-  function searchResultCount() {
-    if(keyWord && cardList.length !== 0)
-      return <span className="font-pre text-lg-r text-gray-400">“{keyWord}”님을 총 {cardList.length}명 찾았습니다.</span>
-  }
+  useEffect(() => {
+    fetchSearchProfiles();
+  }, [fetchSearchProfiles]);
+
+  const searchResultCount = useCallback(async () => {
+    if (keyWord && cardList.length !== 0)
+      return (
+        <span className="font-pre text-lg-r text-gray-400">
+          “{keyWord}”님을 총 {cardList.length}명 찾았습니다.
+        </span>
+      );
+  }, [cardList.length, keyWord]);
 
   function handleSearchBar(e: React.KeyboardEvent<HTMLInputElement>) {
-    if(e.key === 'Enter')
-      setkeyWord(e.currentTarget.value);
+    if (e.key === 'Enter') setkeyWord(e.currentTarget.value);
   }
 
   function handlePageChange(page: number) {
@@ -60,17 +67,20 @@ const WikiList = () => {
   }
 
   function showCardList() {
-    if(isLoading)
+    if (isLoading)
       return (
         <div className="mt-[57px] h-[474px] flex flex-col gap-[24px]">
-          <Card name="" code="" city="" nationality="" job=""/>
-          <Card name="" code="" city="" nationality="" job=""/>
-          <Card name="" code="" city="" nationality="" job=""/>
+          <Card name="" code="" city="" nationality="" job="" />
+          <Card name="" code="" city="" nationality="" job="" />
+          <Card name="" code="" city="" nationality="" job="" />
         </div>
       );
 
-    if(cardList.length > 0) {
-      const currentCardList = cardList.slice(currentPage*3 - 3, currentPage*3);
+    if (cardList.length > 0) {
+      const currentCardList = cardList.slice(
+        currentPage * 3 - 3,
+        currentPage * 3
+      );
 
       return (
         <>
@@ -88,7 +98,13 @@ const WikiList = () => {
             ))}
           </div>
           <div className="mt-[81px] mb-[229px] md:mt-[121px] md:mb-[136px]">
-            { <Pagination totalPages={totalPage} currentPage={currentPage} onPageChange={handlePageChange} /> }
+            {
+              <Pagination
+                totalPages={totalPage}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+              />
+            }
           </div>
           <div className="card_layout_change"></div>
         </>
@@ -97,34 +113,42 @@ const WikiList = () => {
       return (
         <div className="mt-[204px] flex flex-col items-center gap-[32px]">
           <div>
-            <p className="font-pre text-xl-m text-gray-400">"{keyWord}"과 일치하는 검색 결과가 없어요.</p>
+            <p className="font-pre text-xl-m text-gray-400">
+              "{keyWord}"과 일치하는 검색 결과가 없어요.
+            </p>
           </div>
-          { GraySearchIcon({ size: 185 }) }
+          {GraySearchIcon({ size: 185 })}
         </div>
       );
     }
   }
-  
+
   useEffect(() => {
     fetchSearchProfiles();
     searchResultCount();
-  },[keyWord]);
+  }, [fetchSearchProfiles, keyWord, searchResultCount]);
 
   useEffect(() => {
     fetchGetProfiles();
-  },[])
+  }, []);
 
   return (
     <div className="mt-[60px] px-[24px] w-[100%] md:mt-[80px] md:mx-auto md:w-[859px] md:px-[0px]">
       <div className="w-[100%] flex flex-col items-center">
         <div className="flex flex-col w-[100%] gap-[20px] md:gap-[16px]">
-          <div><SearchBar className="w-[100%] h-[45px]" placeholder="검색어를 입력해 주세요." onKeyDown={handleSearchBar} /></div>
-          { searchResultCount() }
+          <div>
+            <SearchBar
+              className="w-[100%] h-[45px]"
+              placeholder="검색어를 입력해 주세요."
+              onKeyDown={handleSearchBar}
+            />
+          </div>
+          {searchResultCount()}
         </div>
-        { showCardList() }
+        {showCardList()}
       </div>
     </div>
   );
-}
+};
 
 export default WikiList;
