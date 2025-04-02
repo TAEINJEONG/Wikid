cbe4df425194eebacb553920b28cf5cb9b6f10cb
TAEINJEONG <taeinjeong46@gmail.com>
2025-04-02 19:28:28 +0900

    fix: mypage 수정 및 파비콘 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,23 @@ import { SnackbarProvider } from '@/lib/context/SnackbarContext';
 import { AuthProvider } from '@/lib/context/authProvider';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import favicon from '@/assets/images/favicon.png';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <SnackbarProvider>
-        <Header />
-        <Component {...pageProps} />
-      </SnackbarProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel="icon" href={favicon.src} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <AuthProvider>
+        <SnackbarProvider>
+          <Header />
+          <Component {...pageProps} />
+        </SnackbarProvider>
+      </AuthProvider>
+    </>
   );
 }
